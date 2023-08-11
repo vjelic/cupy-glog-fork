91dc0ddb794e3f675397c0dec4a79c83d3cb5eae
lcskrishna <lollachaitanya@gmail.com>
2023-08-11 08:20:09 +0000

    update versioning for hiprtcGetBitcode and BitcodeSize

--- cupy_backends/hip/cupy_hiprtc.h
+++ cupy_backends/hip/cupy_hiprtc.h
@@ -3,4 +3,19 @@
 
 #include <hip/hiprtc.h>
 
+extern "C" {
+
+#if HIP_VERSION < 50631061 
+
+hiprtcResult hiprtcGetBitcode(hiprtcProgram prog, char *bitcode) {
+    return HIPRTC_ERROR_COMPILATION;
+}
+
+hiprtcResult hiprtcGetBitcodeSize(hiprtcProgram prog, size_t *bitcode_size) {
+    return HIPRTC_ERROR_COMPILATION;
+}
+#endif
+
+} // extern "C"
+
 #endif // #ifndef INCLUDE_GUARD_HIP_CUPY_HIPRTC_H
