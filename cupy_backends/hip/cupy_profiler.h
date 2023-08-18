1dace030cdb21f2fe8ad724551bf0aa4c7a227ec
Prasanth Nunna <Prasanth.Nunna@amd.com>
2023-08-18 21:13:22 +0000

    Remove hipProfiler deprecated functions

--- cupy_backends/hip/cupy_profiler.h
+++ cupy_backends/hip/cupy_profiler.h
@@ -12,14 +12,6 @@ hipError_t hipProfilerInitialize(...) {
     return hipSuccess;
 }
 
-void hipProfilerStart() {
-    return roctracer_start();
-}
-
-void hipProfilerStop() {
-    return roctracer_stop();
-}
-
 } // extern "C"
 
 #endif // #ifndef INCLUDE_GUARD_HIP_CUPY_PROFILER_H
