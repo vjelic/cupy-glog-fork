3b2264bbd38f31ca1482167c6f4e0f846b7a99e5
bmedishe <bmedishe@amd.com>
2023-11-14 20:03:40 +0000

    update from code review comments

--- .github/workflows/pretest-rocm-test.sh
+++ .github/workflows/pretest-rocm-test.sh
@@ -2,15 +2,20 @@
 
 set -uex
 
+# Python 3.8 (Ubuntu 20.04)
 apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get -y install python3.9-dev python3-pip
+DEBIAN_FRONTEND=noninteractive apt-get -y install python3-pip python3-dev git
 
 hipconfig
 
-python3.9 -m pip install -U pip wheel
+pip3 install -U pip wheel
+pip3 install cython
+
+# install hipify_torch
+pip3 install git+https://github.com/ROCmSoftwarePlatform/hipify_torch.git
 
 export ROCM_HOME="/opt/rocm"
 export HCC_AMDGPU_TARGET="gfx900"
 export CUPY_INSTALL_USE_HIP="1"
-python3.9 -m pip install -v -e .
-python3.9 -c "import cupy; cupy.show_config()"
+pip3 install -v -e .
+python3 -c "import cupy; cupy.show_config()"
