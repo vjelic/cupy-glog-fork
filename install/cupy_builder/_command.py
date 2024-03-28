a3cefaf0fa1f77e57d148220874b6fae46218f62
Prasanth Nunna <Prasanth.Nunna@amd.com>
2024-03-28 14:02:42 +0000

    Define DONT_USE env var for cuda and stub

--- install/cupy_builder/_command.py
+++ install/cupy_builder/_command.py
@@ -123,6 +123,7 @@ class custom_build_ext(setuptools.command.build_ext.build_ext):
             compile_time_env['CUPY_CUDA_VERSION'] = 0
             compile_time_env['CUPY_HIP_VERSION'] = 0
             compile_time_env['CUPY_USE_GEN_HIP_CODE'] = 0
+            compile_time_env['CUPY_DONT_USE_GEN_HIP_CODE'] = 1
         elif ctx.use_hip:  # on ROCm/HIP
             compile_time_env['CUPY_CUDA_VERSION'] = 0
             compile_time_env['CUPY_HIP_VERSION'] = build.get_hip_version()
@@ -133,6 +134,7 @@ class custom_build_ext(setuptools.command.build_ext.build_ext):
                 ctx.features['cuda'].get_version())
             compile_time_env['CUPY_HIP_VERSION'] = 0
             compile_time_env['CUPY_USE_GEN_HIP_CODE'] = 0
+            compile_time_env['CUPY_DONT_USE_GEN_HIP_CODE'] = 1
 
         print('Compile-time constants: ' +
               json.dumps(compile_time_env, indent=4))
