4284c7abc8bf435235cf4b44c0732f1a0af39229
shbiswas834 <shreyashri.biswas@amd.com>
2024-06-11 21:00:48 +0000

    build-rocm test fix for roctx.h

--- cupy_backends/hip/cupy_roctx.h
+++ cupy_backends/hip/cupy_roctx.h
@@ -10,19 +10,68 @@
 
 extern "C" {
 
+// Define NVTX compatible types and functions if NVTX is not available
+typedef enum nvtxColorType_t {
+    NVTX_COLOR_UNKNOWN  = 0,
+    NVTX_COLOR_ARGB     = 1
+} nvtxColorType_t;
+
+typedef enum nvtxMessageType_t {
+    NVTX_MESSAGE_UNKNOWN          = 0,
+    NVTX_MESSAGE_TYPE_ASCII       = 1,
+    NVTX_MESSAGE_TYPE_UNICODE     = 2,
+} nvtxMessageType_t;
+
+typedef union nvtxMessageValue_t {
+    const char* ascii;
+    const wchar_t* unicode;
+} nvtxMessageValue_t;
+
+typedef struct nvtxEventAttributes_v1 {
+    uint16_t version;
+    uint16_t size;
+    uint32_t category;
+    int32_t colorType;
+    uint32_t color;
+    int32_t payloadType;
+    int32_t reserved0;
+    union payload_t {
+        uint64_t ullValue;
+        int64_t llValue;
+        double dValue;
+    } payload;
+    int32_t messageType;
+    nvtxMessageValue_t message;
+} nvtxEventAttributes_v1;
+
+typedef nvtxEventAttributes_v1 nvtxEventAttributes_t;
+
 // Fallback functions for NVTX using ROCtx
 
 void nvtxMarkA(const char* message) {
     roctxMarkA(message);
 }
 
+void nvtxMarkEx(const nvtxEventAttributes_t* attrib) {
+    roctxMarkA(attrib->message.ascii); // ROCtx doesn't support attributes, only message
+}
+
 int nvtxRangePushA(const char* message) {
     return roctxRangePushA(message);
+}
+
+int nvtxRangePushEx(const nvtxEventAttributes_t* attrib) {
+    return roctxRangePushA(attrib->message.ascii); // ROCtx doesn't support attributes, only message
 }
 
 int nvtxRangePop() {
     return roctxRangePop();
 }
+
+uint64_t nvtxRangeStartEx(const nvtxEventAttributes_t* attrib) {
+    return roctxRangePushA(attrib->message.ascii); // Using range push as ROCtx does not have start
+}
+
 
 } // extern "C"
 
