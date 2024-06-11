b085246c5818687f63058f4c7b7f7ac747545101
shbiswas834 <shreyashri.biswas@amd.com>
2024-06-11 20:28:42 +0000

    cuda build fail test

--- cupy_backends/cupy_tx.h
+++ cupy_backends/cupy_tx.h
@@ -16,6 +16,46 @@
 
 extern "C" {
 
+typedef enum nvtxColorType_t
+{
+    NVTX_COLOR_UNKNOWN  = 0,
+    NVTX_COLOR_ARGB     = 1
+} nvtxColorType_t;
+
+typedef enum nvtxMessageType_t
+{
+    NVTX_MESSAGE_UNKNOWN          = 0,
+    NVTX_MESSAGE_TYPE_ASCII       = 1,
+    NVTX_MESSAGE_TYPE_UNICODE     = 2,
+} nvtxMessageType_t;
+
+typedef union nvtxMessageValue_t
+{
+    const char* ascii;
+    const wchar_t* unicode;
+} nvtxMessageValue_t;
+
+typedef struct nvtxEventAttributes_v1
+{
+    uint16_t version;
+    uint16_t size;
+    uint32_t category;
+    int32_t colorType;
+    uint32_t color;
+    int32_t payloadType;
+    int32_t reserved0;
+    union payload_t
+    {
+        uint64_t ullValue;
+        int64_t llValue;
+        double dValue;
+    } payload;
+    int32_t messageType;
+    nvtxMessageValue_t message;
+} nvtxEventAttributes_v1;
+
+typedef nvtxEventAttributes_v1 nvtxEventAttributes_t;
+
 void nvtxMarkA(...) {
 }
 
@@ -25,6 +65,20 @@ int nvtxRangePushA(...) {
 
 int nvtxRangePop() {
     return 0;
+}
+
+void nvtxMarkEx(...) {
+}
+
+int nvtxRangePushEx(...) {
+    return 0;
+}
+
+uint64_t nvtxRangeStartEx(...) {
+    return 0;
+}
+
+void nvtxRangeEnd(...) {
 }
 
 }
