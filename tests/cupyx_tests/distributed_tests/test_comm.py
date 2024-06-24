4a5e1ca5d422af4866b15d61c7f4c29fc7682db7
Prasanth Nunna <Prasanth.Nunna@amd.com>
2024-06-24 14:23:36 +0000

    Skip Non MPI tests for ROCm

--- tests/cupyx_tests/distributed_tests/test_comm.py
+++ tests/cupyx_tests/distributed_tests/test_comm.py
@@ -14,13 +14,29 @@ from cupy import testing
 from cupyx.distributed import init_process_group
 from cupyx.distributed._nccl_comm import _mpi_available
 
+import functools
 
 nccl_available = nccl.available
 
+def decorate_ifnot(cls_name, deco):
+    '''This decorator applies deco if method is not an instance of cls_name
+       The condition is checked only if its ROCm environment,
+       otherwise deco is applied directly'''
+    def decorater(method):
+        @functools.wraps(method)
+        def wrapper(instance, *args, **kwargs):
+            if runtime.is_hip and instance.__class__.__name__==cls_name:
+                return method(instance, *args, **kwargs)
+            else:
+                return deco(method)(instance, *args, **kwargs)
+        return wrapper 
+    return decorater
 
 def _run_test(test_name, dtype=None):
     # subprocess is required not to interfere with cupy module imported in top
     # of this file
+    if runtime.is_hip:
+        pytest.skip('ROCm/HIP may have a bug') 
     runner_path = pathlib.Path(__file__).parent / 'comm_runner.py'
     args = [sys.executable, runner_path, test_name, 'store']
     if dtype is not None:
@@ -54,55 +70,53 @@ def _run_test_with_mpi(test_name, dtype=None):
 
 
 @pytest.mark.skipif(not nccl_available, reason='nccl is not installed')
-@pytest.mark.skipif(runtime.is_hip, reason='ROCm/HIP may have a bug')
 @testing.multi_gpu(2)
 class TestNCCLBackend:
     def _run_test(self, test, dtype):
         _run_test(test, dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_broadcast(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_broadcast(self, dtype=None):
         self._run_test('broadcast', dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_reduce(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_reduce(self, dtype=None):
         self._run_test('reduce', dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_all_reduce(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_all_reduce(self, dtype=None):
         self._run_test('all_reduce', dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_reduce_scatter(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_reduce_scatter(self, dtype=None):
         self._run_test('reduce_scatter', dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_all_gather(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_all_gather(self, dtype=None):
         self._run_test('all_gather', dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_send_and_recv(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_send_and_recv(self, dtype=None):
         self._run_test('send_and_recv', dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_send_recv(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_send_recv(self, dtype=None):
         self._run_test('send_recv', dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_scatter(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_scatter(self, dtype=None):
         self._run_test('scatter', dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_gather(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_gather(self, dtype=None):
         self._run_test('gather', dtype)
 
-    @testing.for_all_dtypes(no_bool=True)
-    def test_all_to_all(self, dtype):
+    @decorate_ifnot('TestNCCLBackend', testing.for_all_dtypes(no_bool=True))
+    def test_all_to_all(self, dtype=None):
         self._run_test('all_to_all', dtype)
 
     def test_barrier(self):
         self._run_test('barrier', None)
-
 
 @pytest.mark.skipif(not _mpi_available, reason='mpi is not installed')
 @testing.multi_gpu(2)
@@ -112,50 +126,49 @@ class TestNCCLBackendWithMPI(TestNCCLBackend):
 
 
 @pytest.mark.skipif(not nccl_available, reason='nccl is not installed')
-@pytest.mark.skipif(runtime.is_hip, reason='ROCm/HIP may have a bug')
 @testing.multi_gpu(2)
 class TestNCCLBackendSparse:
     def _run_test(self, test, dtype):
         _run_test(test, dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_send_and_recv(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_send_and_recv(self, dtype=None):
         self._run_test('sparse_send_and_recv', dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_broadcast(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_broadcast(self, dtype=None):
         self._run_test('sparse_broadcast', dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_reduce(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_reduce(self, dtype=None):
         self._run_test('sparse_reduce', dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_all_reduce(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_all_reduce(self, dtype=None):
         self._run_test('sparse_all_reduce', dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_scatter(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_scatter(self, dtype=None):
         self._run_test('sparse_scatter', dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_gather(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_gather(self, dtype=None):
         self._run_test('sparse_gather', dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_all_gather(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_all_gather(self, dtype=None):
         self._run_test('sparse_all_gather', dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_all_to_all(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_all_to_all(self, dtype=None):
         self._run_test('sparse_all_to_all', dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_reduce_scatter(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_reduce_scatter(self, dtype=None):
         self._run_test('sparse_reduce_scatter', dtype)
 
-    @testing.for_dtypes('fdFD')
-    def test_send_recv(self, dtype):
+    @decorate_ifnot('TestNCCLBackendSparse', testing.for_dtypes('fdFD'))
+    def test_send_recv(self, dtype=None):
         self._run_test('sparse_send_recv', dtype)
 
 
@@ -169,8 +182,8 @@ class TestNCCLBackendSparseWithMPI(TestNCCLBackendSparse):
 @pytest.mark.skipif(not nccl_available, reason='nccl is not installed')
 class TestInitDistributed(unittest.TestCase):
 
-    @pytest.mark.skipif(runtime.is_hip, reason='ROCm/HIP may have a bug')
     @testing.multi_gpu(2)
+    @pytest.mark.skipif(runtime.is_hip, reason='ROCm/HIP may have a bug')
     def test_init(self):
         _run_test('init')
 
